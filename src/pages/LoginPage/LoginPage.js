9ab4629bc0d8e1c318180006c0776260372b38a9
Melissa <melmihomel@gmail.com>
2022-10-18 10:13:29 -0300

    primeiros ajustes

---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,10 @@
+import logo from "../../assets/images/Group 8.png"
+
+export default function LoginPage() {
+    return(
+        <>
+        <img src={logo} alt="track-it" />
+        
+        </>
+    )
+}
---
+++ src/pages/SIgnUpPage.js/SIgnUpPage.js
@@ -0,0 +1,13 @@
+import logo from "../../assets/images/Group 8.png"
+
+
+export default function SignUpPage(){
+
+    return(
+        <>
+            <p>cadastro</p>
+            <img src={logo} alt="track-it" />
+
+        </>
+    )
+}
